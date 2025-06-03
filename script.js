88f4413b90df0df20dbae81933af5c6d38fbfefb
Red2011 <pet.loxtv2@outlook.com>
2025-06-03 22:44:17 +1000

    Add cake and message

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-$(function(){
-    if (!$('.envelope').hasClass('open')){
-        $('.envelope').click(function(){
-            $(this).removeClass('new').addClass('open');
-        });
-    }
+$(function () {
+    $('.envelope').on('click', function () {
+        const $envelope = $(this);
+
+        if (!$envelope.hasClass('open')) {
+            $envelope.removeClass('new').addClass('open');
+            const $letter = $envelope.find('.letter');
+            $letter.hide().fadeIn(300, function () {
+                setTimeout(function () {
+                    $letter.addClass('reveal');
+                }, 1450);
+            });
+        }
+    });
 });
